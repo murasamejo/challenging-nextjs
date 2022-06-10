b64df90b73217962f2c59c9cc838620d262da417
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-06-10 22:44:06 +0900

    feat: 🎸 Auth0 の基礎の基礎を実装した

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,11 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <div>
+          <p><a href="/api/auth/login">Login</a></p>
+          <p><a href="/api/auth/logout">Logout</a></p>
+        </div>
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
